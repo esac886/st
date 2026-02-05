a47f9979fa48c6e2c793d45ff3d32207a7c79b9f
Kirill Rusnik <rusni1kk@gmail.com>
2026-02-05 16:15:36 +0700

    Changed ctrl-↑/↓ keymaps to ctrl-shift-n/p

--- config.h
+++ config.h
@@ -189,8 +189,8 @@ static Shortcut shortcuts[] = {
     { TERMMOD,      XK_plus,   zoom,          { .f = +1 } },
     { ControlMask,  XK_minus,  zoom,          { .f = -1 } },
     { ControlMask,  XK_equal,  zoomreset,     { .f = 0  } },
-    { ControlMask,  XK_Up,     kscrollup,     { .i = 1  } },
-    { ControlMask,  XK_Down,   kscrolldown,   { .i = 1  } },
+    { TERMMOD,      XK_P,      kscrollup,     { .i = 1  } },
+    { TERMMOD,      XK_N,      kscrolldown,   { .i = 1  } },
     { TERMMOD,      XK_K,      kscrollup,     { .i = -1 } },
     { TERMMOD,      XK_J,      kscrolldown,   { .i = -1 } },
 };
